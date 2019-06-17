987d9ac21fe8cd070716b20c0245cc892d05a568
Ankur Arya <ankur.arya@nagarro.com>
2019-06-17 15:07:33 +0530

    Initial Commit

---
+++ PodDemo/Background.swift
@@ -0,0 +1,31 @@
+//
+//  Background.swift
+//  PodDemo
+//
+//  Created by Ankur Arya on 17/06/19.
+//  Copyright © 2019 Ankur Arya. All rights reserved.
+//
+
+import UIKit
+
+class Background: UIView {
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        changeColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func changeColor() {
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in  //1
+            UIView.animate(withDuration: 2.0) {  //2
+                self.layer.backgroundColor = self.colors.randomElement()?.cgColor
+            }
+        }
+        scheduledColorChanged.fire()
+    }
+}
